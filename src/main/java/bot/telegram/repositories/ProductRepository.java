c39002c877249ca83d810804a446034d24ef69db
aeldridg42 <vv.azat@yandex.ru>
2023-03-22 16:31:04 +0300

    first commit

---
+++ src/main/java/bot/telegram/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package bot.telegram.repositories;
+
+import bot.telegram.models.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
---
+++ src/test/java/bot/telegram/TgBotForSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotForSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
