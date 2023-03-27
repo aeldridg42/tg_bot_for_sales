234bf51bf94346dcad2417ceb0285bf75b310dca
aeldridg42 <vv.azat@yandex.ru>
2023-03-27 20:54:13 +0300

    webapp added

---
+++ webapp/src/main/java/org/telegram/webapp/controller/ProductController.java
@@ -0,0 +1,7 @@
+package org.telegram.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+}
---
+++ webapp/src/main/java/org/telegram/webapp/model/Product.java
@@ -0,0 +1,33 @@
+package org.telegram.webapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String category;
+    private String description;
+    private double price;
+    private String url;
+    private Long last_updated;
+    private String picture_url;
+
+    @Override
+    public String toString() {
+        return "name: " + name +
+                "\ncategory: " + category +
+                "\ndescription: " + description +
+                "\nprice: " + price +
+                "\npicture: " + picture_url;
+    }
+}
